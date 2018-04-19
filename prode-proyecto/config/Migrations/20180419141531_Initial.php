1b8e1b80042217c81fe9af667c174f8b419bb5a4
Pablo Iglesias <pablo.iglesias02@gmail.com>
2018-04-19 11:55:26 -0300

    Inicializacion del proyecto + migrations base

---
+++ prode-proyecto/config/Migrations/20180419141531_Initial.php
@@ -0,0 +1,76 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+    public function up()
+    {
+
+        $this->table('rol_usuarios', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('nombre', 'string', [
+                'default' => null,
+                'limit' => 50,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+
+        $this->table('users', ['id' => false, 'primary_key' => ['id']])
+            ->addColumn('id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('username', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('rol_usuario_id', 'uuid', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('rol_usuarios');
+        $this->dropTable('users');
+    }
+}
